3b2560914c0eb1082c57303b452824f782a165eb
sebas-luna <plunas@miumg.edu.gt>
2024-09-26 15:41:42 -0600

    comentarios y cambio de models

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
@@ -11,10 +12,12 @@ const homeRoute = require('./homeRoute');
 app.get('/', homeRoute);
 
 app.listen(port, () => {
+    // Inicia el servidor y escucha en el puerto definido.
     console.log(`Example app listening at http://localhost:${port}`);
 });
 
 mongoose.connect('mongodb://localhost:27017/LosPanas').then(() => {
+    // Conecta a la base de datos MongoDB 'LosPanas' en localhost.
     console.log('Database connected');
 }).catch((err) => {
     console.log(err); 
--- models/producto.model.js
+++ models/producto.model.js
@@ -1,10 +1,16 @@
+// Importa mongoose, una biblioteca de MongoDB para modelar datos.
 const mongoose = require('mongoose');
 
+// Un esquema es una estructura que describe cómo se almacenarán los datos en MongoDB.
 const ProductoEsquema = new mongoose.Schema({
     nombre: String,
-    descripcion: String,
     precio: Number,
     cantidad: Number,
+    description: {
+        type: mongoose.Schema.Types.Mixed,
+        required: true
+    },
+        // Este campo es un objeto con varias propiedades que describen las características técnicas del producto.
     caracteristicas: {
         type: mongoose.Schema.Types.Mixed, 
         default: { 
@@ -24,5 +30,5 @@ const ProductoEsquema = new mongoose.Schema({
     }
 });
 
-
+// Esto permite que otras partes de la aplicación utilicen este modelo para realizar operaciones CRUD (Crear, Leer, Actualizar, Eliminar) en la base de datos.
 module.exports = mongoose.model('Producto', ProductoEsquema);
